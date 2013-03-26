c1c2b6b225f6fca67d6f66beda7451f5b24f917d
afraser <afraser@boundlesslearning.com>
2013-03-26 16:16:47 -0400

    try twitter api 1.1

--- javascripts/load_tweets.js
+++ javascripts/load_tweets.js
@@ -1,6 +1,6 @@
 $(function() {
   
-  $.get("https://api.twitter.com/1/statuses/user_timeline.json?include_entities=true&include_rts=true&screen_name=BoundlessDevs&count=5", {},
+  $.get("https://api.twitter.com/1.1/statuses/user_timeline.json?screen_name=BoundlessDevs&count=2", {},
     function(result) {
       console.log(result);
       return $('.tweets').append(result);
