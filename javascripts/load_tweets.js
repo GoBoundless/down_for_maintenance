11aa1dc48637d194d3367d3ff18de7bf4f2795fb
afraser <afraser@boundlesslearning.com>
2013-03-26 16:43:16 -0400

    screw it, use twitter widget

--- javascripts/load_tweets.js
+++
@@ -1,9 +0,0 @@
-$(function() {
-  
-  $.get("https://api.twitter.com/1.1/statuses/user_timeline.json?screen_name=BoundlessDevs&count=2", {},
-    function(result) {
-      console.log(result);
-      return $('.tweets').append(result);
-    }
-  );
-});
